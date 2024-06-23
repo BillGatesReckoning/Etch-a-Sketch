96881991ef1097a9f1fdfefcb1e7d7b62220bc6a
BillGatesReckoning <rusinkost@gmail.com>
2024-06-23 19:42:18 +0200

    Work on 960X960 container still not complete

--- javaScript.js
+++ javaScript.js
@@ -4,7 +4,7 @@ createDivs(16);
 const gridCreateButton = document.querySelector("#gridCreate");
 gridCreateButton.addEventListener("click", () => {
     let gridSize = prompt("Enter your desired grid size")
-    if (gridSize > 1 || gridSize < 101) {
+    if (gridSize > 1 && gridSize < 101) {
         createDivs(gridSize)
     }
     else {
@@ -13,8 +13,26 @@ gridCreateButton.addEventListener("click", () => {
 })
 
 
+function createDivs(size) {
+    deleteGrid()
+    const container = document.querySelector("#container");
+    for (let i = 0; i < size; i++) {
+        const columnDiv = document.createElement("div");
+        columnDiv.setAttribute("class", "columnDiv")
+        for (let j = 0; j < size; j++) {
+            const div = document.createElement("div");
+            div.setAttribute("class", "manyDiv");
+            columnDiv.appendChild(div);
+        }
+        container.appendChild(columnDiv);
+    }
+    addDivAttributes()
+    return
+}
+
+
 function addDivAttributes() {
-    const divs = document.querySelectorAll(".manydiv");
+    const divs = document.querySelectorAll(".manyDiv");
     divs.forEach((div) => {
     div.addEventListener("mouseover", () => {
         div.setAttribute("style", "background-color: pink;")
@@ -25,22 +43,6 @@ function addDivAttributes() {
 })
 }
 
-
-function createDivs(size) {
-    deleteGrid()
-    const container = document.querySelector("#container");
-    for (let i = 0; i < size; i++) {
-        const columnDiv = document.createElement("div");
-        for (let j = 0; j < size; j++) {
-            const div = document.createElement("div");
-            div.setAttribute("class", "manydiv");
-            columnDiv.appendChild(div);
-        }
-        container.appendChild(columnDiv);
-    }
-    addDivAttributes()
-    return
-}
 
 function deleteGrid() {
     const divsToDelete = document.querySelectorAll("div#container > div")
