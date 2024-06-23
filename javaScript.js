2c8acc636384a4da22c56bbfc3643ecbef8fe11d
BillGatesReckoning <rusinkost@gmail.com>
2024-06-23 19:19:12 +0200

    Add option to create your own grid with a button

--- javaScript.js
+++ javaScript.js
@@ -1,11 +1,21 @@
-const size = 16;
+let gridSize = 16;
 createDivs(16);
 
-const gridCreate = document.querySelector("#gridCreate");
+const gridCreateButton = document.querySelector("#gridCreate");
+gridCreateButton.addEventListener("click", () => {
+    let gridSize = prompt("Enter your desired grid size")
+    if (gridSize > 1 || gridSize < 101) {
+        createDivs(gridSize)
+    }
+    else {
+        alert("enter realizable grid")
+    }
+})
 
-const divs = document.querySelectorAll(".manydiv");
 
-divs.forEach((div) => {
+function addDivAttributes() {
+    const divs = document.querySelectorAll(".manydiv");
+    divs.forEach((div) => {
     div.addEventListener("mouseover", () => {
         div.setAttribute("style", "background-color: pink;")
     })
@@ -13,8 +23,11 @@ divs.forEach((div) => {
         div.setAttribute("style", "background-color: white;")
     })
 })
+}
+
 
 function createDivs(size) {
+    deleteGrid()
     const container = document.querySelector("#container");
     for (let i = 0; i < size; i++) {
         const columnDiv = document.createElement("div");
@@ -25,4 +38,13 @@ function createDivs(size) {
         }
         container.appendChild(columnDiv);
     }
+    addDivAttributes()
+    return
+}
+
+function deleteGrid() {
+    const divsToDelete = document.querySelectorAll("div#container > div")
+    divsToDelete.forEach((divToDelete) => {
+        divToDelete.remove()
+    })
 }
